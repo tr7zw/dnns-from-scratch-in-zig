a5b9e0ec8bc93483887ff53843f3ea4193ccf206
jklw10 <112possu@gmail.com>
2024-06-14 20:31:27 +0300

    activation structs unified

--- src/gaussian.zig
+++ src/gaussian.zig
@@ -1,57 +1,53 @@
 const std = @import("std");
 
-pub fn Activation() type {
-    return struct {
-        last_inputs: []const f64 = undefined,
-        fwd_out: []f64,
-        bkw_out: []f64,
-        batchSize: usize,
-        size: usize,
+last_inputs: []const f64 = undefined,
+fwd_out: []f64,
+bkw_out: []f64,
+batchSize: usize,
+size: usize,
 
-        const Self = @This();
-        pub const mu: f64 = 1.0; // center of the bump
-        pub const sigma: f64 = 2.0; // width of the bump
+const Self = @This();
+pub const mu: f64 = 1.0; // center of the bump
+pub const sigma: f64 = 2.0; // width of the bump
 
-        pub fn init(
-            alloc: std.mem.Allocator,
-            batchSize: usize,
-            size: usize,
-        ) !Self {
-            return Self{
-                .last_inputs = try alloc.alloc(f64, size * batchSize),
-                .fwd_out = try alloc.alloc(f64, size * batchSize),
-                .bkw_out = try alloc.alloc(f64, size * batchSize),
-                .batchSize = batchSize,
-                .size = size,
-            };
-        }
-
-        fn gaussian_bump(x: f64) f64 {
-            const exponent = -((x - mu) * (x - mu)) / (2.0 * sigma * sigma);
-            return @exp(exponent);
-        }
-
-        fn gaussian_bump_derivative(x: f64) f64 {
-            const exponent = -((x - mu) * (x - mu)) / (2.0 * sigma * sigma);
-            return -((x - mu) / (sigma * sigma)) * @exp(exponent);
-        }
-
-        pub fn forward(self: *Self, inputs: []f64) void {
-            std.debug.assert(inputs.len == self.size * self.batchSize);
-
-            var i: usize = 0;
-            while (i < inputs.len) : (i += 1) {
-                self.fwd_out[i] = gaussian_bump(inputs[i]);
-            }
-            self.last_inputs = inputs;
-        }
-
-        pub fn backwards(self: *Self, grads: []f64) void {
-            std.debug.assert(grads.len == self.size * self.batchSize);
-            var i: usize = 0;
-            while (i < self.last_inputs.len) : (i += 1) {
-                self.bkw_out[i] = grads[i] * gaussian_bump_derivative(self.last_inputs[i]);
-            }
-        }
+pub fn init(
+    alloc: std.mem.Allocator,
+    batchSize: usize,
+    size: usize,
+) !Self {
+    return Self{
+        .last_inputs = try alloc.alloc(f64, size * batchSize),
+        .fwd_out = try alloc.alloc(f64, size * batchSize),
+        .bkw_out = try alloc.alloc(f64, size * batchSize),
+        .batchSize = batchSize,
+        .size = size,
     };
+}
+
+fn gaussian_bump(x: f64) f64 {
+    const exponent = -((x - mu) * (x - mu)) / (2.0 * sigma * sigma);
+    return @exp(exponent);
+}
+
+fn gaussian_bump_derivative(x: f64) f64 {
+    const exponent = -((x - mu) * (x - mu)) / (2.0 * sigma * sigma);
+    return -((x - mu) / (sigma * sigma)) * @exp(exponent);
+}
+
+pub fn forward(self: *Self, inputs: []f64) void {
+    std.debug.assert(inputs.len == self.size * self.batchSize);
+
+    var i: usize = 0;
+    while (i < inputs.len) : (i += 1) {
+        self.fwd_out[i] = gaussian_bump(inputs[i]);
+    }
+    self.last_inputs = inputs;
+}
+
+pub fn backwards(self: *Self, grads: []f64) void {
+    std.debug.assert(grads.len == self.size * self.batchSize);
+    var i: usize = 0;
+    while (i < self.last_inputs.len) : (i += 1) {
+        self.bkw_out[i] = grads[i] * gaussian_bump_derivative(self.last_inputs[i]);
+    }
 }
--- src/pyramid.zig
+++ src/pyramid.zig
@@ -1,66 +1,62 @@
 const std = @import("std");
 
-pub fn Activation() type {
-    return struct {
-        last_inputs: []const f64 = undefined,
-        fwd_out: []f64,
-        bkw_out: []f64,
-        batchSize: usize,
-        size: usize,
+last_inputs: []const f64 = undefined,
+fwd_out: []f64,
+bkw_out: []f64,
+batchSize: usize,
+size: usize,
 
-        const Self = @This();
+const Self = @This();
 
-        pub const threshold: f64 = 1.0;
-        pub const leak_slope: f64 = 0.01;
+pub const threshold: f64 = 1.0;
+pub const leak_slope: f64 = 0.01;
 
-        pub fn init(
-            alloc: std.mem.Allocator,
-            batchSize: usize,
-            size: usize,
-        ) !Self {
-            return Self{
-                .last_inputs = try alloc.alloc(f64, size * batchSize),
-                .fwd_out = try alloc.alloc(f64, size * batchSize),
-                .bkw_out = try alloc.alloc(f64, size * batchSize),
-                .batchSize = batchSize,
-                .size = size,
-            };
-        }
-
-        pub fn forward(self: *Self, inputs: []f64) void {
-            std.debug.assert(inputs.len == self.size * self.batchSize);
-
-            var i: usize = 0;
-            while (i < inputs.len) : (i += 1) {
-                const x = inputs[i];
-                if (x < 0) {
-                    self.fwd_out[i] = leak_slope * x;
-                } else if (x < threshold) {
-                    self.fwd_out[i] = x;
-                } else if (x < 2 * threshold) {
-                    self.fwd_out[i] = 2 * threshold - x;
-                } else {
-                    self.fwd_out[i] = leak_slope * (x - 2 * threshold);
-                }
-            }
-            self.last_inputs = inputs;
-        }
-
-        pub fn backwards(self: *Self, grads: []f64) void {
-            std.debug.assert(grads.len == self.size * self.batchSize);
-            var i: usize = 0;
-            while (i < self.last_inputs.len) : (i += 1) {
-                const x = self.last_inputs[i];
-                if (x < 0) {
-                    self.bkw_out[i] = leak_slope * grads[i];
-                } else if (x < threshold) {
-                    self.bkw_out[i] = grads[i];
-                } else if (x < 2 * threshold) {
-                    self.bkw_out[i] = -grads[i];
-                } else {
-                    self.bkw_out[i] = leak_slope * grads[i];
-                }
-            }
-        }
+pub fn init(
+    alloc: std.mem.Allocator,
+    batchSize: usize,
+    size: usize,
+) !Self {
+    return Self{
+        .last_inputs = try alloc.alloc(f64, size * batchSize),
+        .fwd_out = try alloc.alloc(f64, size * batchSize),
+        .bkw_out = try alloc.alloc(f64, size * batchSize),
+        .batchSize = batchSize,
+        .size = size,
     };
+}
+
+pub fn forward(self: *Self, inputs: []f64) void {
+    std.debug.assert(inputs.len == self.size * self.batchSize);
+
+    var i: usize = 0;
+    while (i < inputs.len) : (i += 1) {
+        const x = inputs[i];
+        if (x < 0) {
+            self.fwd_out[i] = leak_slope * x;
+        } else if (x < threshold) {
+            self.fwd_out[i] = x;
+        } else if (x < 2 * threshold) {
+            self.fwd_out[i] = 2 * threshold - x;
+        } else {
+            self.fwd_out[i] = leak_slope * (x - 2 * threshold);
+        }
+    }
+    self.last_inputs = inputs;
+}
+
+pub fn backwards(self: *Self, grads: []f64) void {
+    std.debug.assert(grads.len == self.size * self.batchSize);
+    var i: usize = 0;
+    while (i < self.last_inputs.len) : (i += 1) {
+        const x = self.last_inputs[i];
+        if (x < 0) {
+            self.bkw_out[i] = leak_slope * grads[i];
+        } else if (x < threshold) {
+            self.bkw_out[i] = grads[i];
+        } else if (x < 2 * threshold) {
+            self.bkw_out[i] = -grads[i];
+        } else {
+            self.bkw_out[i] = leak_slope * grads[i];
+        }
+    }
 }
